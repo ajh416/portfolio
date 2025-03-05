c892ada12800d72697ec652cfdaa0ecd00ccbe81
ajh416 <henryada@oregonstate.edu>
2025-03-04 17:47:19 -0800

    initial commit of portfolio (mostly done)

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,56 @@
-import Image from "next/image";
+import styles from './page.module.css';
+import Link from 'next/link';
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className={styles.body}>
+      <Head>
+	<title>Adam Henry</title>
+      </Head>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Hey, I'm Adam Henry</h1>
+      <p className={styles.intro}>
+        I’m a programmer who builds cool stuff.<br></br>Passionate about cool things like images and machine vision. 
+      </p>
+      <h1 className={styles.title}>Stuff I've Made</h1>
+      <div className={styles.cardGrid}>
+        <div className={styles.card}>
+          <img src="images/wildfire-map.png" alt="Image of wildfire-map.com, displaying a map of current wildfires" className={styles.cardImage} />
+	  <div className={styles.cardHeader}>
+            <h3>Wildfire Map</h3>
+            <Link href="https://wildfire-map.com" target="_blank" className={styles.button}>View</Link>
+          </div>
+          <p>Displaying wildfire locations and information on an interactive map!</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className={styles.card}>
+          <img src="images/raytracer.png" alt="A ray-traced image featuring multiple objects and light" className={styles.cardImage} />
+	  <div className={styles.cardHeader}>
+            <h3>Ray/Path Tracer</h3>
+            <Link href="https://github.com/ajh416/RayTracer" target="_blank" className={styles.button}>View</Link>
+          </div>
+          <p>A ray/path tracer created from scratch using well-known ray-object intersection techniques.</p>
+        </div>
+      </div>
+
+      <div className={styles.resumeSection}>
+        <h2 className={styles.resumeTitle}>About Me</h2>
+        <div className={styles.resumeContent}>
+          <h3>Education</h3>
+          <p>Oregon State University - Computer Science, Expected June 2025</p>
+          <p>Relevant Courses: Data Structures (CS261), Computer Graphics (CS450), Computer Graphics Shaders (CS457), Web Development (CS290)</p>
+          <h3>Skills</h3>
+          <ul className={styles.skillsList}>
+            <li>C/C++ and OpenGL</li>
+            <li>JavaScript, HTML, CSS</li>
+            <li>Python and data analysis</li>
+          </ul>
+          <h3>Hobbies</h3>
+          <p>Programming, gaming, hiking, and I've been learning about radio/rf stuff.</p>
+        </div>
+	<p className={styles.resumePlug}>Check out my <Link href="/resume.pdf" target="_blank" className={styles.resumebutton}>resume (.pdf)</Link> for all of my information.</p>
+      </div>
+    </div>
     </div>
   );
 }
